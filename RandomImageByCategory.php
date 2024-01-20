936ea1137e48a035f785512ad832bdeaa87fec94
Célestin Métral <celestin.metral@pm.me>
2024-01-20 11:15:00 +0000

    Switched to parser function and edited random image selection mode

--- RandomImageByCategory.php
+++ RandomImageByCategory.php
@@ -1,14 +1,15 @@
 <?php
 /**
  * RandomImageByCategory extension
- * Usage example: <randomimagebycategory width="200" categories="Featured Image"/>
- * Supported parameters: width, limit, categories
+ * Usage example: {{#randomimagebycategory: category=ImageVitrine | size=300px | position=centré| vignette}}
+ * Supported parameters: position, size, category, position
  *
  * @file
  * @ingroup Extensions
  * @author Aaron Wright <aaron.wright@gmail.com>
  * @author David Pean <david.pean@gmail.com>
  * @author Jack Phoenix
+ * @author Célestin Métral
  * @link https://www.mediawiki.org/wiki/Extension:RandomImageByCategory Documentation
  * @license GPL-2.0-or-later
  */
@@ -17,26 +18,22 @@ use MediaWiki\MediaWikiServices;
 
 class RandomImageByCategory {
 
-	public static function registerTag( &$parser ) {
-		$parser->setHook( 'randomimagebycategory', [ __CLASS__, 'getRandomImage' ] );
+	public static function registerTag( Parser $parser ) {
+		$parser->setFunctionHook( 'randomimagebycategory', [ self::class, 'getRandomImage' ] );
 	}
 
-	public static function getRandomImage( $input, $args, $parser ) {
+	public static function getRandomImage( Parser $parser) {
 		$parser->getOutput()->updateCacheExpiry( 0 );
 
-		$categories = ( isset( $args['categories'] ) ) ? trim( $args['categories'] ) : '';
+		$options = extractOptions( array_slice( func_get_args(), 1 ) );
 
-		if ( isset( $args['limit'] ) && is_numeric( $args['limit'] ) ) {
-			$limit = $args['limit'];
-		} else {
-			$limit = 10;
-		}
+		$category = ( isset( $options["category"] ) ) ? $options["category"] : 'ImageVitrine';
+		$size = ( isset ($options["size"]) ) ? $options["size"] : "300px";
+		$position = ( isset ($options["position"]) ) ? $options["position"] : "centré";
+		$vignette = $options["vignette"] ?  "|vignette" : "";
 
-		if ( isset( $args['width'] ) && is_numeric( $args['width'] ) ) {
-			$width = $args['width'];
-		} else {
-			$width = 200;
-		}
+		# Limitation du nombre d’image récupérées au numéro du jour puisque nous allons prendre l’image correspondant au numéro du jour
+		$limit = date('j');
 
 		$services = MediaWikiServices::getInstance();
 		$cache = $services->getMainWANObjectCache();
@@ -46,7 +43,7 @@ class RandomImageByCategory {
 
 		if ( !$data ) {
 			wfDebug( "Getting random image list from DB\n" );
-			$ctg = $parser->replaceVariables( $categories );
+			$ctg = $parser->replaceVariables( $category );
 			$ctg = $parser->getStripState()->unstripBoth( $ctg );
 			$ctg = str_replace( "\,", '#comma#', $ctg );
 			$aCat = explode( ',', $ctg );
@@ -88,17 +85,45 @@ class RandomImageByCategory {
 
 		$random_image = '';
 		$thumbnail = '';
+		# Récupération de l’image correspondant au numéro du jour (avec un modulo s’il n’y a pas assez d’images)
 		if ( count( $image_list ) > 0 ) {
-			$random_image = $image_list[ array_rand( $image_list, 1 ) ];
+			$random_image = $image_list[ date('j') % sizeof($image_list) ];
+			#$random_image = $image_list[ array_rand( $image_list, 1 ) ];
 		}
 
 		if ( $random_image ) {
 			$image_title = Title::makeTitle( NS_FILE, $random_image );
-			$render_image = $services->getRepoGroup()->findFile( $random_image );
-			$thumb_image = $render_image->transform( [ 'width' => $width ] );
-			$thumbnail = "<a href=\"" . htmlspecialchars( $image_title->getFullURL() ) . "\">{$thumb_image->toHtml()}</a>";
+			#$render_image = $services->getRepoGroup()->findFile( $random_image );
+			#$thumb_image = $render_image->transform( [ 'width' => $width ] );
+			#$thumbnail = "<a href=\"" . htmlspecialchars( $image_title->getFullURL() ) . "\">{$thumb_image->toHtml()}</a>";
+			#$thumbnail = htmlspecialchars( $image_title->getPartialURL() );
+			$thumbnail = "[[Fichier:" . htmlspecialchars( $image_title->getPartialURL() ). "|". $position ."|". $size ."|alt=L’image du jour : ".$image_title . $vignette ."|link=".  htmlspecialchars( $image_title->getFullURL() ) ."|L’image du jour]]";
+
 		}
 
 		return $thumbnail;
 	}
+}
+
+
+/**
+ * Converts an array of values in form [0] => "name=value"
+ * into a real associative array in form [name] => value
+ * If no = is provided, true is assumed like this: [name] => true
+ *
+ * @param array string $options
+ * @return array $results
+ */
+function extractOptions( array $options ) {
+	$results = [];
+	foreach ( $options as $option ) {
+		$pair = array_map( 'trim', explode( '=', $option, 2 ) );
+		if ( count( $pair ) === 2 ) {
+			$results[ $pair[0] ] = $pair[1];
+		}
+		if ( count( $pair ) === 1 ) {
+			$results[ $pair[0] ] = true;
+		}
+	}
+	return $results;
 }
